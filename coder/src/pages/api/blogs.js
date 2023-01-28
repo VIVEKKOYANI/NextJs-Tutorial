cfde2e7dabcad53870ba919f110ad835000ab48a
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-01-29 00:09:26 +0530

    show all blog dynemic

--- coder/src/pages/api/blogs.js
+++ coder/src/pages/api/blogs.js
@@ -3,8 +3,15 @@ import * as fs from 'fs';
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", "utf-8", (err, data) => {
-    res.status(200).json(data)
-  })
+export default async function handler(req, res) {
+
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allBlogs = [];
+  for (let index = 0; index < data.length; index++) {
+    const item = data[index];
+    myfile = await fs.promises.readFile(('blogdata/' + item), 'utf-8')
+    allBlogs.push(JSON.parse(myfile))
+  }
+  res.status(200).json(allBlogs)
 }
--- coder/src/pages/blog.js
+++ coder/src/pages/blog.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import styles from '@/styles/Home.module.css'
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link';
+import styles from '@/styles/Blog.module.css'
 
 function blog() {
+
+  const [blogs, setblogs] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/blogs').then((a) => {
+      return a.json();
+    }).then((parsed) => {
+      setblogs(parsed)
+    })
+  }, [])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className="blogItem">
-          <h1>How to learn JavaScript in 2022?</h1>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
-        <div className="blogItem">
-          <h1>How to learn JavaScript in 2022?</h1>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
-        <div className="blogItem">
-          <h1>How to learn JavaScript in 2022?</h1>
-          <p>JavaScript is the language used to design logic for the web</p>
-        </div>
+        {blogs.map((blogItem) => {
+          return (
+            <div className="blogItem" key={blogItem.slug}>
+              <Link href={`/blogpost/${blogItem.slug}`} />
+              <h1 className={styles.blogitem3}>{blogItem.title}</h1>
+              <p className={styles.blogitemp}>{blogItem.content.substr(0,140)}...</p>
+            </div>
+          )
+        })}
       </main>
     </div>
   )
