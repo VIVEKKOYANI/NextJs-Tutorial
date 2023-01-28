d5867b3dc87b4f2bf9836c32d301dd0946b9d6d5
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-01-28 22:11:20 +0530

    add next.js

--- coder/src/pages/blog.js
+++ coder/src/pages/blog.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from '@/styles/Home.module.css'
 
 function blog() {
   return (
-    <div>blog</div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className="blogItem">
+          <h1>How to learn JavaScript in 2022?</h1>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h1>How to learn JavaScript in 2022?</h1>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h1>How to learn JavaScript in 2022?</h1>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+      </main>
+    </div>
   )
 }
 
