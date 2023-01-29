bef6435bec2e9604f8287554b8572bffb52ddd3d
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-01-29 22:14:24 +0530

    add getblog api chnages and show data in front

--- coder/src/pages/api/getblog.js
+++ coder/src/pages/api/getblog.js
@@ -4,10 +4,10 @@ import * as fs from 'fs';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  fs.readFile(`blogdata/${req.query.slug}`, "utf-8", (err, data) => {
+  fs.readFile(`./blogdata/${req.query.slug}.json`, "utf-8", (err, data) => {
     if(err){
         res.status(500).json({error: "No such blog found"})    
     }
-    res.status(200).json(JSON.parse(data))
+    res.status(200).json(data)
   })
 }
--- coder/src/pages/blogpost/[slug].js
+++ coder/src/pages/blogpost/[slug].js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
+import styles from '../../styles/BlogPost.module.css';
 
 function slug() {
 
-    const router = useRouter();
-    const {slug} = router.query;
+  const [blog, setblog] = useState([]);
+  const router = useRouter();
+  const { slug } = router.query;
+
+  useEffect(() => {
+    console.log("routerisReady", !router.isReady);
+    if(!router.isReady) return;
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+      return a.json();
+    }).then((parsed) => {
+      setblog(parsed)
+    })
+  }, [router.isReady])
+
 
   return (
-    <div>{slug}</div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>{blog.title}</h1>
+        <hr />
+        <div>
+          {blog.content}
+        </div>
+      </main>
+    </div>
   )
 }
 
