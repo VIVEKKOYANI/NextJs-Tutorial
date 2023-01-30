408bd6df65d72f2f35c51829c8e5f1468db0e1e1
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2023-01-30 23:34:05 +0530

    add server side

--- coder/src/pages/blogpost/[slug].js
+++ coder/src/pages/blogpost/[slug].js
@@ -2,21 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router';
 import styles from '../../styles/BlogPost.module.css';
 
-function slug() {
+function slug(props) {
 
-  const [blog, setblog] = useState([]);
+  const [blog, setblog] = useState(props.myBlog);
   const router = useRouter();
-  const { slug } = router.query;
 
-  useEffect(() => {
-    console.log("routerisReady", !router.isReady);
-    if(!router.isReady) return;
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
-      return a.json();
-    }).then((parsed) => {
-      setblog(parsed)
-    })
-  }, [router.isReady])
+  // useEffect(() => {
+  //   console.log("routerisReady", !router.isReady);
+  //   if(!router.isReady) return;
+  //   fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a) => {
+  //     return a.json();
+  //   }).then((parsed) => {
+  //     setblog(parsed)
+  //   })
+  // }, [router.isReady])
 
 
   return (
@@ -30,6 +29,17 @@ function slug() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const { slug } = router.query;
+
+  let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  let myBlog = await data.json()
+
+  return{
+    props: {myBlog},
+  }
 }
 
 export default slug;
